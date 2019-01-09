2be94eac1d8aaa737aa8ced7221511bc415f182c
Oscar <oscarwasil@gmail.com>
2019-01-08 19:44:08 -0600

    Added to comp and some html

--- src/index.js
+++ src/index.js
@@ -7,14 +7,24 @@ class Square extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: null,
+      squares: Array(9).fill(null),
     };
   }
 
 
+  renderSquare(i) {
+    return <Square value={i} />;
+  }
 
+  render() {
+    const status = "Next player: X";
+  }
 
+  return (
+    <div>
 
+    </div>
+    );
 
   render() {
     return (
